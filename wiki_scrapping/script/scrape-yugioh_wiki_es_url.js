afee5ccb7ee54bd18ff7241f423ad27f3c074f9d
Adonis Beltrán <thexbestxplayer@hotmail.com>
2017-06-06 13:07:13 -0400

    completado el script de conexión al db, el de búsqueda de url y el de actualización de nombre y texto

---
+++ wiki_scrapping/script/scrape-yugioh_wiki_es_url.js
@@ -0,0 +1,37 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('cards.cdb');
+var check;
+db.serialize(function() {
+
+  db.each("SELECT id, english_name, jap_name, set_card FROM cartas WHERE id=35699", function(err, row) {
+  
+  /*console.log(row.id + " " + row.jap_name);*/
+  var search = "";
+  if(row.jap_name == ""){
+	search = row.english_name;
+  }else{
+	search = row.jap_name;
+  }
+    
+request("http://es.yugioh.wikia.com/wiki/Especial:Buscar?search=" +  encodeURIComponent(search), function(error, response, body) {
+  if(error) {
+    console.log("Error: " + error);
+  }
+  console.log("Status code: " + response.statusCode);
+  
+  var $ = cheerio.load(body);
+  
+  $('ul.Results').each(function( index ) {
+        	  
+	  var spanish_name = $(this).find('a.result-link[data-pos="1"]').first().text().trim();
+      var url = $(this).find('a.result-link[data-pos="1"]').last().text().trim();
+	  var stmt = db.prepare("UPDATE cartas SET spanish_name = ?, url = ?  WHERE id=?;");
+	  stmt.run(spanish_name, url, row.id);
+		 	  
+  });
+  });
+ });
+});
